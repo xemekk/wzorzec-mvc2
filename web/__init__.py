d54d4b44565ce3493fbe5281d5afc83fbcd7207b
xemekk <xemronn@gmail.com>
2022-04-09 20:17:57 +0200

    folder structure changes before deployment

--- web/__init__.py
+++ web/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = ''
+    app.config['SECRET_KEY'] = 'c7c7d9f9b7ffada7ec48becbba734c70aa32b81882c6130a16306fffc68c8e15'
     
     from .views import views
     from .auth import auth
